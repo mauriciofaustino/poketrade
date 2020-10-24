a0f4e082e1074ff3beadcddc0084a053c400988a
Mauricio Castro <mauricio.faustino@gmail.com>
2020-10-24 10:59:47 -0300

    Adicionado condicao de Justica da troca

---
+++ src/main/java/com/mauriciofaustino/poketrade/PoketradeApplication.java
@@ -0,0 +1,13 @@
+package com.mauriciofaustino.poketrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauriciofaustino/poketrade/PoketradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauriciofaustino.poketrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
